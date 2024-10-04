50d97d146a97bb12882b9b2242f8ecaaba1fff52
elwalker627 <elwalker627@gmail.com>
2024-10-03 22:36:48 -0600

    Updated to food

--- app/page.tsx
+++ app/page.tsx
@@ -27,13 +27,13 @@ export default function App() {
 
   function createTodo() {
     client.models.Todo.create({
-      content: window.prompt("Todo content"),
+      content: window.prompt("Food name"),
     });
   }
 
   return (
     <main>
-      <h1>My todos</h1>
+      <h1>What I've cooked:</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
@@ -41,11 +41,8 @@ export default function App() {
         ))}
       </ul>
       <div>
-        🥳 App successfully hosted. Try creating a new todo.
+        🥳 App successfully hosted. Try adding a new food.
         <br />
-        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-          Review next steps of this tutorial.
-        </a>
       </div>
     </main>
   );
